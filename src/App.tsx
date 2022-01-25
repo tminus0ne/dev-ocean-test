61c0c2d87e71ee4a967dd5da0e7363bf3cfd1225
Greg-One <sgmdoc@yandex.ru>
2022-01-25 21:03:42 +0500

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello React!</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ webpack/webpack.common.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
+          'css-loader',
+          'sass-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src',
+          to: 'build',
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
